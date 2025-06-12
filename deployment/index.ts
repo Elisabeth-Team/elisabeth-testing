9ab5fbf7451cd733a3f09156489edbd757944838
Elisabeth Lichtie <elisabethlichtie99@gmail.com>
2025-06-12 14:34:08 -0500

    asdf

--- deployment/index.ts
+++ deployment/index.ts
@@ -22,7 +22,7 @@ if (imagePath) {
 // Upload the index.html to the bucket
 const indexHtml = new aws.s3.BucketObject("index.html", {
   bucket: bucket.id,
-  content: `<html><header>Hi, Check this out</header><body><img src="image1.jpg" /></body></html>`,
+  content: `<html><header>Hi, Check these out</header><body><img src="image1.jpg" /></body></html>`,
   contentType: "text/html",
 });
 
